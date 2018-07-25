5a1ee0c8458c54dd60054607ea769ceaf9cd655c
xareas <xareas@gmail.com>
2018-07-25 07:01:45 -0600

    BRIDGE

---
+++ Patrones/Bridge/DataObject.cs
@@ -0,0 +1,12 @@
+﻿namespace Patrones.Bridge
+{
+    public abstract class DataObject
+    {
+        public abstract void NextRecord();
+        public abstract void PriorRecord();
+        public abstract void AddRecord(string name);
+        public abstract void DeleteRecord(string name);
+        public abstract void ShowRecord();
+        public abstract void ShowAllRecords();
+    }
+}
